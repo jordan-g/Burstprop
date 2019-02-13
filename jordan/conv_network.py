a68c58d3c787da6da2acae5c34016ef56e8958cd
Jordan Guerguiev <jordan.guerguiev@me.com>
2019-02-13 12:39:56 -0500

    Shuffle training data every epoch, add my conv net

--- jordan/conv_network.py
+++ jordan/conv_network.py
@@ -6,7 +6,7 @@ from cifar10 import cifar10
 import torch
 import torch.optim as optim
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.autograd import Variable
 
 import pdb
 
@@ -66,6 +66,10 @@ def update(x, d, parameters, state, gradients, hyperparameters):
     b_t   = state["b_t"]
     delta = gradients["delta"]
 
+    state["conv_layers_optimizer"].zero_grad()
+    state["conv_layers_output"] = parameters["conv_layers"].forward(x)
+    x = state["conv_layers_output"].data
+
     for i in range(hyperparameters["num_hidden_layers"]+1):
         if i == 0:
             e[i] = torch.sigmoid(W[i].mm(x) + bias[i])
@@ -94,10 +98,10 @@ def update(x, d, parameters, state, gradients, hyperparameters):
             else:
                 c = 0
 
-            u = Y[i].mm(b[i+1]*(1 - e[i+1])) - c
+            u = Y[i+1].mm(b[i+1]*(1 - e[i+1])) - c
 
             p[i]   = torch.sigmoid(hyperparameters["beta"]*u)
-            p_t[i] = torch.sigmoid(hyperparameters["beta"]*(Y[i].mm(b_t[i+1]*(1 - e[i+1])) - c))
+            p_t[i] = torch.sigmoid(hyperparameters["beta"]*(Y[i+1].mm(b_t[i+1]*(1 - e[i+1])) - c))
 
             b[i]    = p[i]*e[i]
             b_t[i]  = p_t[i]*e[i]
@@ -119,12 +123,20 @@ def update(x, d, parameters, state, gradients, hyperparameters):
             if hyperparameters["use_recurrent_input"]:
                 gradients["Z"][i] = -u.mm(e[i].transpose(0, 1))
 
+    if hyperparameters["use_backprop"]:
+        gradients["conv"] = W[0].transpose(0, 1).mm(delta[0])
+    else:
+        u   = W[0].transpose(0, 1).mm(b[0]*(1 - e[0]))
+        u_t = W[0].transpose(0, 1).mm(b_t[0]*(1 - e[0]))
+
+        gradients["conv"] = u - u_t
+
     return state, gradients, train_cost, train_error
 
 def update_weights(parameters, state, gradients, hyperparameters):
     for i in range(hyperparameters["num_hidden_layers"]+1):
-        parameters["delta_W"][i]    = -hyperparameters["forward_learning_rates"][i]*gradients["W"][i] + hyperparameters["momentum"]*parameters["delta_W"][i]
-        parameters["delta_bias"][i] = -hyperparameters["forward_learning_rates"][i]*gradients["bias"][i] + hyperparameters["momentum"]*parameters["delta_bias"][i]
+        parameters["delta_W"][i]    = -hyperparameters["forward_learning_rates"][i+1]*gradients["W"][i] + hyperparameters["momentum"]*parameters["delta_W"][i]
+        parameters["delta_bias"][i] = -hyperparameters["forward_learning_rates"][i+1]*gradients["bias"][i] + hyperparameters["momentum"]*parameters["delta_bias"][i]
 
         parameters["W"][i]    += parameters["delta_W"][i] - hyperparameters["weight_decay"]*parameters["W"][i]
         parameters["bias"][i] += parameters["delta_bias"][i]
@@ -135,12 +147,15 @@ def update_weights(parameters, state, gradients, hyperparameters):
             parameters["Z"][i] += parameters["delta_Z"][i]
 
     if hyperparameters["symmetric_weights"]:
-        parameters["Y"]  = [ parameters["W"][i+1].transpose(0, 1).clone() for i in range(0, hyperparameters["num_hidden_layers"]) ]
+        parameters["Y"]  = [ parameters["W"][i].transpose(0, 1).clone() for i in range(0, hyperparameters["num_hidden_layers"])+1 ]
     elif hyperparameters["same_sign_weights"]:
         for i in range(0, hyperparameters["num_hidden_layers"]):
-            mask = (np.sign(parameters["Y"][i]) != np.sign(parameters["W"][i+1].transpose(0, 1))).type(dtype_byte)
-            parameters["Y"][i][mask] = parameters["W"][i+1].transpose(0, 1)[mask]
+            mask = (torch.sign(parameters["Y"][i]) != torch.sign(parameters["W"][i].transpose(0, 1))).type(dtype_byte)
+            parameters["Y"][i][mask] = parameters["W"][i].transpose(0, 1)[mask]
 
+    state["conv_layers_output"].backward(gradient=gradients["conv"])
+    state["conv_layers_optimizer"].step()
+    
     return parameters
 
 def test(x, d, parameters, hyperparameters):
@@ -149,11 +164,15 @@ def test(x, d, parameters, hyperparameters):
 
     e = [ [] for i in range(hyperparameters["num_hidden_layers"]+1) ]
 
+    x = parameters["conv_layers"].forward(x).data
+
     for i in range(hyperparameters["num_hidden_layers"]+1):
         if i == 0:
             e[i] = torch.sigmoid(W[i].mm(x) + bias[i])
         else:
             e[i] = torch.sigmoid(W[i].mm(e[i-1]) + bias[i])
+
+    # print(e[-1])
 
     test = 100.0*int(torch.sum(torch.ne(torch.max(e[-1], 0)[1], torch.max(d, 0)[1])))/x.shape[1]
 
@@ -163,6 +182,10 @@ def initialize(hyperparameters):
     num_hidden_layers = hyperparameters["num_hidden_layers"]
     num_hidden_units  = hyperparameters["num_hidden_units"]
 
+    conv_layers           = ConvLayers(dataset=hyperparameters["dataset"])
+    conv_layers_criterion = nn.MSELoss()
+    conv_layers_optimizer = optim.SGD(conv_layers.parameters(), lr=hyperparameters["forward_learning_rates"][0], momentum=hyperparameters["momentum"], weight_decay=hyperparameters["weight_decay"])
+
     if hyperparameters["dataset"] == "mnist":
         input_size  = 784
         output_size = 10
@@ -170,35 +193,43 @@ def initialize(hyperparameters):
         input_size  = 1024
         output_size = 10
 
+    x          = torch.zeros((input_size, 1)).type(dtype)
+    outputs    = conv_layers.forward(x)
+    input_size = conv_layers.output_size
+
     num_units  = [input_size] + num_hidden_units + [output_size]
     num_layers = num_hidden_layers + 2
 
     W_range = [ np.sqrt(6/(num_units[i-1] + num_units[i])) for i in range(1, num_layers) ]
 
     parameters = {
-        "W":          [ torch.from_numpy(np.random.uniform(-W_range[i-1], W_range[i-1], size=(num_units[i], num_units[i-1]))).type(dtype) for i in range(1, num_layers) ],
-        "bias":       [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
-        "Z":          [ torch.from_numpy(np.random.uniform(-hyperparameters["Z_range"][i-1], hyperparameters["Z_range"][i-1], size=(num_units[i], num_units[i]))).type(dtype) for i in range(1, num_layers-1) ],
-        "delta_W":    [ torch.from_numpy(np.zeros((num_units[i], num_units[i-1]))).type(dtype) for i in range(1, num_layers) ],
-        "delta_bias": [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
-        "delta_Z":    [ torch.from_numpy(np.zeros((num_units[i], num_units[i]))).type(dtype) for i in range(1, num_layers-1) ]
+        "W":           [ torch.from_numpy(np.random.uniform(-W_range[i-1], W_range[i-1], size=(num_units[i], num_units[i-1]))).type(dtype) for i in range(1, num_layers) ],
+        "bias":        [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
+        "Z":           [ torch.from_numpy(np.random.uniform(-hyperparameters["Z_range"][i-1], hyperparameters["Z_range"][i-1], size=(num_units[i], num_units[i]))).type(dtype) for i in range(1, num_layers-1) ],
+        "delta_W":     [ torch.from_numpy(np.zeros((num_units[i], num_units[i-1]))).type(dtype) for i in range(1, num_layers) ],
+        "delta_bias":  [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
+        "delta_Z":     [ torch.from_numpy(np.zeros((num_units[i], num_units[i]))).type(dtype) for i in range(1, num_layers-1) ],
+        "conv_layers": conv_layers
     }
 
     if hyperparameters["symmetric_weights"]:
-        parameters["Y"] = [ parameters["W"][i+1].transpose(0, 1).clone() for i in range(0, num_layers-2) ]
+        parameters["Y"] = [ parameters["W"][i].transpose(0, 1).clone() for i in range(0, num_layers-1) ]
     else:
-        parameters["Y"] = [ torch.from_numpy(np.random.uniform(-hyperparameters["Y_range"][i], hyperparameters["Y_range"][i], size=(num_units[i+1], num_units[i+2]))).type(dtype) for i in range(0, num_layers-2) ]
+        parameters["Y"] = [ torch.from_numpy(np.random.uniform(-hyperparameters["Y_range"][i], hyperparameters["Y_range"][i], size=(num_units[i], num_units[i+1]))).type(dtype) for i in range(0, num_layers-1) ]
 
         if hyperparameters["same_sign_weights"]:
-            for i in range(0, hyperparameters["num_hidden_layers"]):
-                mask = (np.sign(parameters["Y"][i]) != np.sign(parameters["W"][i+1].transpose(0, 1))).type(dtype_byte)
-                parameters["Y"][i][mask] = parameters["W"][i+1].transpose(0, 1)[mask]
+            for i in range(0, hyperparameters["num_hidden_layers"]+1):
+                mask = (torch.sign(parameters["Y"][i]) != torch.sign(parameters["W"][i].transpose(0, 1))).type(dtype_byte)
+                parameters["Y"][i][mask] = parameters["W"][i].transpose(0, 1)[mask]
+    
     state = {
-        "e":   [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
-        "p":   [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
-        "p_t": [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
-        "b":   [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
-        "b_t": [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ]
+        "e":                     [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
+        "p":                     [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
+        "p_t":                   [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
+        "b":                     [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
+        "b_t":                   [ torch.from_numpy(np.zeros((num_units[i], 1))).type(dtype) for i in range(1, num_layers) ],
+        "conv_layers_criterion": conv_layers_criterion,
+        "conv_layers_optimizer": conv_layers_optimizer
     }
 
     gradients = {
@@ -210,3 +241,57 @@ def initialize(hyperparameters):
     }
 
     return parameters, state, gradients
+
+class ConvLayers(nn.Module):
+    def __init__(self, dataset):
+        super(ConvLayers, self).__init__()
+        self.output_size = None
+        self.dataset     = dataset
+        
+        if dataset == "mnist":
+            self.conv1 = nn.Conv2d(1, 8, 5)
+            self.pool1 = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(8, 16, 5)
+            self.pool2 = nn.MaxPool2d(2, 2)
+
+            torch.nn.init.xavier_uniform_(self.conv1.weight)
+            torch.nn.init.xavier_uniform_(self.conv2.weight)
+        elif dataset == "cifar10":
+            self.conv1 = nn.Conv2d(3, 8, 5, stride=1, padding=2)
+            self.pool1 = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(8, 16, 5, stride=1, padding=2)
+            self.pool2 = nn.MaxPool2d(2, 2)
+            self.conv3 = nn.Conv2d(16, 20, 5, stride=1, padding=2)
+            self.pool3 = nn.MaxPool2d(2, 2)
+
+            torch.nn.init.xavier_uniform_(self.conv1.weight)
+            torch.nn.init.xavier_uniform_(self.conv2.weight)
+            torch.nn.init.xavier_uniform_(self.conv3.weight)
+
+        if cuda:
+            self.cuda()
+
+    def forward(self, x):
+        batch_size = x.size()[1]
+
+        if self.dataset == "mnist":
+            x = self.pool1(torch.sigmoid(self.conv1(Variable(x.transpose(0, 1)).contiguous().view(batch_size, 1, 28, 28))))
+            x = self.pool2(torch.sigmoid(self.conv2(x)))
+        elif self.dataset == "cifar10":
+            x = self.pool1(torch.sigmoid(self.conv1(Variable(x.transpose(0, 1)).contiguous().view(batch_size, 3, 32, 32))))
+            x = self.pool2(torch.sigmoid(self.conv2(x)))
+            x = self.pool3(torch.sigmoid(self.conv3(x)))
+
+        self.output_size = self.flat_output_size(x)
+        x = x.view(batch_size, self.output_size).transpose(0, 1)
+
+        return x
+
+    def flat_output_size(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+
+        flat_output_size = 1
+        for s in size:
+            flat_output_size *= s
+
+        return flat_output_size
--- main.py
+++ main.py
@@ -7,23 +7,23 @@ parser.add_argument("-model", help='Model to use.', default='jordan')
 parser.add_argument("-data_directory", help='Directory in which to save data.')
 parser.add_argument("-dataset", type=str, help="Which dataset to train on ('mnist' or 'cifar10').", default="mnist")
 parser.add_argument("-num_epochs", type=int, help="Number of epochs.", default=10)
-parser.add_argument("-num_hidden_layers", type=int, help="Number of hidden layers.", default=1)
+parser.add_argument("-num_hidden_layers", type=int, help="Number of hidden layers.", default=2)
 parser.add_argument("-batch_size", type=int, help="Batch size.", default=1)
 parser.add_argument("-test_frequency", type=int, help="Frequency (ie. every ___ batches) at which to save network state & get test error.", default=1000)
-parser.add_argument("-num_hidden_units", help="Number of units in each layer.", type=lambda s: [int(item) for item in s.split(',')], default=[500])
-parser.add_argument("-Z_range", help="Range of uniform distribution used for initial recurrent weights.", type=lambda s: [float(item) for item in s.split(',')], default=[0.01])
-parser.add_argument("-Y_range", help="Range of uniform distribution used for initial feedback weights.", type=lambda s: [float(item) for item in s.split(',')], default=[1.0])
-parser.add_argument("-forward_learning_rates", help="Feedforward learning rates.", type=lambda s: [float(item) for item in s.split(',')], default=[0.05, 0.01])
-parser.add_argument("-recurrent_learning_rates", help="Recurrent learning rates.", type=lambda s: [float(item) for item in s.split(',')], default=[0.0001])
-parser.add_argument("-gamma", type=float, help="Output layer burst probability in the absence of a target.", default=1)
+parser.add_argument("-num_hidden_units", help="Number of units in each hidden layer.", type=lambda s: [int(item) for item in s.split(',')], default=[500, 500])
+parser.add_argument("-Z_range", help="Range of uniform distribution used for initial recurrent weights.", type=lambda s: [float(item) for item in s.split(',')], default=[0.01, 0.01])
+parser.add_argument("-Y_range", help="Range of uniform distribution used for initial feedback weights.", type=lambda s: [float(item) for item in s.split(',')], default=[1.0, 1.0])
+parser.add_argument("-forward_learning_rates", help="Feedforward learning rates.", type=lambda s: [float(item) for item in s.split(',')], default=[1.0, 1.0, 1.0])
+parser.add_argument("-recurrent_learning_rates", help="Recurrent learning rates.", type=lambda s: [float(item) for item in s.split(',')], default=[0.0001, 0.0001])
+parser.add_argument("-gamma", type=float, help="Output layer burst probability in the absence of a target.", default=0.2)
 parser.add_argument("-beta", type=float, help="Slope of the burst probability function.", default=1.0)
 parser.add_argument("-symmetric_weights", type=bool, help="Whether to use symmetric weights.", default=False)
-parser.add_argument("-same_sign_weights", type=bool, help="Whether to use feedback weights that are the same sign as feedforward weights.", default=True)
+parser.add_argument("-same_sign_weights", type=bool, help="Whether to use feedback weights that are the same sign as feedforward weights.", default=False)
 parser.add_argument("-use_recurrent_input", type=bool, help="Whether to use recurrent input at hidden layers.", default=True)
 parser.add_argument("-momentum", type=float, help="Momentum", default=0.8)
 parser.add_argument("-weight_decay", type=float, help="Weight decay", default=0.0)
 parser.add_argument("-use_validation", type=bool, help="Whether to use the validation set.", default=False)
-parser.add_argument("-use_backprop", default=True, type=lambda x: (str(x).lower() == 'true'))
+parser.add_argument("-use_backprop", default=False, type=lambda x: (str(x).lower() == 'true'))
 
 args = parser.parse_args()
 
@@ -56,6 +56,8 @@ def initialize_network(hyperparameters):
 	global net
 	if args.model == 'jordan':
 		import jordan.network as net
+	elif args.model == 'jordan_conv':
+		import jordan.conv_network as net
 
 	parameters, state, gradients = net.initialize(hyperparameters)
 
@@ -72,21 +74,25 @@ def train(x_train, d_train, x_test, d_test, parameters, state, gradients, hyperp
 
 	print("Initial {} error: {:.2f}%.".format("validation" if hyperparameters["use_validation"] else "test", test_error[0]))
 
+	example_indices = np.arange(x_train.shape[1])
+
 	for epoch_num in range(hyperparameters["num_epochs"]):
 		for batch_num in range(num_batches):
+			batch_example_indices = example_indices[batch_num*hyperparameters["batch_size"]:(batch_num+1)*hyperparameters["batch_size"]]
+
 			total_batch_num = epoch_num*num_batches + batch_num
 
-			x = x_train[:, batch_num*hyperparameters["batch_size"]:(batch_num+1)*hyperparameters["batch_size"]]
-			d = d_train[:, batch_num*hyperparameters["batch_size"]:(batch_num+1)*hyperparameters["batch_size"]]
+			x = x_train[:, batch_example_indices]
+			d = d_train[:, batch_example_indices]
 
 			state, gradients, train_cost[total_batch_num], train_error[total_batch_num] = net.update(x, d, parameters, state, gradients, hyperparameters)
 
-			parameters = net.update_weights(parameters, gradients, hyperparameters)
+			parameters = net.update_weights(parameters, state, gradients, hyperparameters)
 
 			if (batch_num+1) % hyperparameters["test_frequency"] == 0:
 				test_error[total_batch_num+1] = net.test(x_test, d_test, parameters, hyperparameters)
 
-				print("Epoch {}, batch {}/{}. {} error: {:.2f}%.".format(epoch_num+1, batch_num+1, num_batches, "Validation" if hyperparameters["use_validation"] else "Test", test_error[total_batch_num+1]))
+				print("Epoch {}, batch {}/{}. {} error: {:.2f}%. Train cost: {}.".format(epoch_num+1, batch_num+1, num_batches, "Validation" if hyperparameters["use_validation"] else "Test", test_error[total_batch_num+1], np.mean(train_cost[total_batch_num+1-hyperparameters["test_frequency"]:total_batch_num+1])))
 
 	return train_cost, train_error, test_error
 
